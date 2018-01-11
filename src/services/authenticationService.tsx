23a831f38e0520f9ba73552b63ca6d481c876dfc
Adam Zaręba <adam.zareba@softwarehut.pl>
2018-01-11 14:40:41 +0100

    backend url to constants

--- src/services/authenticationService.tsx
+++ src/services/authenticationService.tsx
@@ -1,18 +1,17 @@
 import axios from 'axios';
-
-const BASE_URL = 'http://localhost:9000';
+import { BACKEND_BASE_URL } from '../config/constants';
 
 export module authenticationService {
 
     export function login(username: string, password: string) {
-        return axios.post(`${BASE_URL}/api/auth/signin/credentials`, {
+        return axios.post(`${BACKEND_BASE_URL}/api/auth/signin/credentials`, {
             'identifier': username,
             'password': password
         }).then(result => result.data);
     }
 
     export function register(username: string, password: string, email: string, name: string, surname: string) {
-        return axios.post(`${BASE_URL}/api/auth/signup`, {
+        return axios.post(`${BACKEND_BASE_URL}/api/auth/signup`, {
             'identifier': username,
             'password': password,
             'email': email,
--- src/services/exampleDataService.tsx
+++ src/services/exampleDataService.tsx
@@ -1,16 +1,15 @@
 import axios from 'axios';
-
-const BASE_URL = 'http://localhost:9000';
+import { BACKEND_BASE_URL } from '../config/constants';
 
 export module exampleDataService {
 
     export function getColors() {
-        const url = `${BASE_URL}/colors`;
+        const url = `${BACKEND_BASE_URL}/colors`;
         return axios.get(url).then(response => response.data);
     }
 
     export function getBadPassword(token: string) {
-        const url = `${BASE_URL}/badPassword`;
+        const url = `${BACKEND_BASE_URL}/badPassword`;
         return axios.get(url, {
             headers: {
                 'X-Auth-Token': token
